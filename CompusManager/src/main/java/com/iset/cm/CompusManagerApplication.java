793fd04f0ce21712e627e3016e490ea6abbbd150
Hammami <Hammami@DESKTOP-JFBPQ58>
2018-10-16 18:29:39 +0100

    first commit

---
+++ CompusManager/src/main/java/com/iset/cm/CompusManagerApplication.java
@@ -0,0 +1,12 @@
+package com.iset.cm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompusManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompusManagerApplication.class, args);
+	}
+}
